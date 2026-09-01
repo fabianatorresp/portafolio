a180b7689854e7efbabd77a66d3144a78022d66c
fabiana <fabianatp146@gmail.com>
2026-09-01 17:04:29 -0400

    agregar archivo javaScrypt

---
+++ main.js
@@ -0,0 +1,20 @@
+const anio = document.querySelector(".fechaHoy")
+anio.textContent = new Date().getFullYear()
+
+/* let date = new Date().getHours()
+const h1 = document.querySelector("h1")
+
+if (date<12) {
+    h1.textContent = "buenos dias"
+}
+else if (date<19) {
+    h1.textContent = "buenas tardes"
+}
+else {
+    h1.textContent = "buenas noches"
+} */
+
+
+/* const etiqueta = document.querySelector("#cumple")
+etiqueta.style.backgroundColor = "blue" */
+
